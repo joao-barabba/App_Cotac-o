00d31a580f1e26ccf6d3205663bb9103bfffa387
escreva o seu nome <jvbarabba@gmail.com>
2021-08-11 22:09:22 -0300

    :bug: Atualizações no arquivo js porém ainda não funcional.

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 
-const url = "https://economia.awesomeapi.com.br/last/USD-BRL"
+const input_to_amount = document.getElementById('to_amount')
+const to_dollar = document.querySelector('.response')
+const moeda = {"USD": "Dollar","BRL": "Real"}
 
-const  cotar = () =>{
-    alert(`Valor inserido ${valor}`)
+async function calculate(){
+    let from = moeda[1]
+    let to = moeda[0]
+    let { rates } = await getURL(`https://api.exchangerate-api.com/v4/latest/${from}`)
+    let rate = rates[to]
+    to_dollar.innerHTML = `O valor em Dolar agora é ${input_to_amount.value}`
 }
 
-const input_to_amount = document.getElementById('to_amount')
-const  input_from_amount =document.getElementById('from_amount')                                     
 
-input_from_amount.addEventListener('click',cotar)
+function main() {
+    calculate() 
+}
+async function getURL(url){
+    return(await fetch(url)).json()
+}
+
+main()
+
+
+
+input_to_amount.addEventListener('input',calculate())
+to_dollar.addEventListener('click', calculate())
+                          
+console.log(from) 
+//input_from_amount.addEventListener('input',cotar())
 
